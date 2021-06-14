0f44fc52c8656ae48590752e5d13e240b11f174d
Qianchen Li <44011064+LeeGeon-jin@users.noreply.github.com>
2021-06-14 11:40:35 +1100

    Add files via upload

--- client/checkout.php
+++ client/checkout.php
@@ -1,5 +1,49 @@
 <?php
 
+$discount_amount=0;
+
+if(isset($_POST["voucher"]))
+{
+    require_once "../dbcontroller/DBManager.php";
+    $db=new DBManager();
+
+
+    $v_name=$_POST["voucher"];
+    $sql_check_name="select * from voucher where v_name='$v_name'";
+    $row=$db->displayDB($sql_check_name)->fetch_assoc();
+    if(empty($row))
+    {
+        echo "<script>alert('优惠券名错误！');</script>";
+    }
+    else
+    {
+        if($row["expire_date"]=="0000-00-00" || $row["expire_date"] < date("Y-m-d"))
+        {
+            $discount_amount=$row["v_discount"];
+        }
+        else
+        {
+            echo "<script>alert('优惠券已过期！');</script>";
+        }
+    }
+}
+
+$final_price=null;
+
+if(isset($_POST["item-price"]))
+{
+    if(isset($_POST["voucher"]))
+    {$final_price = $_POST["item-price"] - $_POST["voucher"];}
+    else
+    {$final_price=$_POST["item-price"];}
+}
+elseif (isset($_SESSION["order-price"]))
+{
+    if(isset($_POST["voucher"]))
+    {$final_price=$_SESSION["order-price"] - $_POST["voucher"];}
+    else
+    {$final_price=$_SESSION["order-price"];}
+}
 ?>
 <html>
 <head>
@@ -7,7 +51,7 @@
 </head>
 <body>
 <h1>结算</h1>
-<form action="added_order.php" method="post">
+<form id="main-form" action="added_order.php" method="post">
     <h2>客户详情</h2>
     <div>
         <label>姓*</label>
@@ -68,16 +112,16 @@
         </tr>
         <tr>
             <th>合计</th>
-            <th><?php echo $_POST["item-price"] ?? "" ?></th>
+            <th><?php echo $final_price ?></th>
         </tr>
     </table>
-    <input id="price" name="price" type="hidden" value="<?php echo $_POST['item-price'] ?? '' ?>">
+    <input id="price" name="price" type="hidden" value="<?php echo $final_price ?? '' ?>">
     <input id="iname" name="iname" type="hidden" value="<?php echo $_POST['item-name'] ?? '' ?>">
     <br>
     <h2>支付方式</h2>
     <div>
         <div>
-            <input id="offline" name="payment" type="radio" value="offline">
+            <input id="offline" name="payment" type="radio" value="offline" required>
             <label>平台外支付</label>
             <p>用手机APP或ATM机直接付款到我们的银行账户。请使用订单号作为付款备注。我们会在资金到账后配送订单。</p>
         </div>
@@ -92,10 +136,16 @@
             <p>仅支持Visa和MasterCard，暂不支持银联卡。</p>
         </div>
     </div>
-
-    <br>
-    <input type="submit" value="下单">
 </form>
+
+<h2>优惠券</h2>
+<form action="" method="post">
+    <input id="voucher" name="voucher" type="text" placeholder="优惠券号">
+    <input type="submit" value="使用">
+</form>
+<br>
+
+<input type="submit" form="main-form" value="下单">
 
 </body>
 </html>
--- client/confirmed_order.php
+++ client/confirmed_order.php
@@ -1,1 +1,43 @@
 <?php
+require_once "../dbcontroller/DBManager.php";
+$db=new DBManager();
+
+$customerReference=$_POST['customerReference'];
+$txnreference=$_POST['txnreference'];
+$responseCode=$_POST['responseCode'];
+$responseText=$_POST['responseText'];
+$amount=$_POST['amount'];
+
+echo "<h3>Payment Details 支付详情</h3>";
+echo "<p>Customer Reference 用户账单号码: ".$customerReference."</p>";
+echo "<p>Txn Reference TXN号码: ".$txnreference."</p>";
+echo "<p>Response Code 返回代码: ".$responseCode."</p>";
+echo "<p>Response Text 返回信息: ".$responseText."</p>";
+echo "<p>Amount 金额: A$".$amount."</p>";
+echo "<br><br>";
+
+
+if($responseCode=="08")
+{
+    $sql="update orders set paid=1 where order_id='$customerReference'";
+    $db->updateDB($sql,"orders");
+    $db->closeConn();
+
+    echo "<h1>Payment Success!</h1>";
+    echo "<h1>支付成功！我们的客服会尽快联系您。</h1>";
+    echo "<a href='https://www.ausimmi.com.au/zh-hans'>Back to homepage 返回主页</a>";
+}
+else
+{
+    echo "<h1>Payment Failed!</h1>";
+    echo "<h1>支付失败！</h1>";
+
+    echo "<form id='data-form' action='https://www.ipg.stgeorge.com.au/StgWeb/servlet/webpay.website.Simple?token1=0sxwHsM4n9rhtxdg4BKzK5qmY&token2=Ym59BlFOi2suaThaepVZrcP8I' method='post'>".
+        "<input id='customerReference' name='customerReference' type='hidden' value='".$customerReference."'>".
+        "<input id='amount' name='amount' type='hidden' value='".$amount."'>".
+        "</form>";
+
+    echo "<button onclick=\"document.getElementById('data-form').submit();\">Try again 重试</button>";
+}
+
+?>
